55d2c729e46640172a2e68ed6f5ec7f8c20682e2
Jean M. Lescure <jean@toptal.com>
2017-07-11 05:12:52 -0600

    Failing user sign-up tests

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -3,17 +3,34 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
   
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
+  it('should allow user to sign up', async () => {
+    await expect(element(by.id('signUpButton'))).toBeVisible();
+    await element(by.id('signUpButton')).tap();
+
+    // Type the Email
+    await element(by.type('RCTTextField')).atIndex(4).typeText('test@example.com');
+    // Type the Password
+    await element(by.type('RCTTextField')).atIndex(3).typeText('test1234');
+    // Type the Password confirmation
+    await element(by.type('RCTTextField')).atIndex(2).typeText('test1234');
+    // Type the First name
+    await element(by.type('RCTTextField')).atIndex(1).typeText('Zaphod');
+
+    // The following makes sure that the Last name field is visible
+    // otherwise the keyboard occludes it and Detox fails the test
+    await element(by.text('Email')).tap();
+    await element(by.type('RCTTextField')).atIndex(0).tap();
+
+    // Type the Last name
+    await element(by.type('RCTTextField')).atIndex(0).typeText('Beeblebrox');
+
+    // Again, the keyboard occludes the Sign Up button
+    // The following hides the keyboard
+    await element(by.text('Email')).tap();
+
+    // Now tap the Sign Up button
+    await element(by.text('Sign Up')).atIndex(1).tap();
+
+    await expect(element(by.text('Coming Soon'))).toBeVisible();
   });
-  
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.label('Hello!!!'))).toBeVisible();
-  });
-  
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.label('World!!!'))).toBeVisible();
-  });
-})
+});
--- e2e/init.js
+++ e2e/init.js
@@ -1,8 +1,39 @@
+require('dotenv').config();
 require('babel-polyfill');
+
+const path = require('path');
 const detox = require('detox');
 const config = require('../package.json').detox;
 
+// Initialize Firebase admin SDK
+const admin = require('firebase-admin');
+const serviceAccount = require(path.join('..', process.env.ADMIN_JSON));
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+});
+
+// Function which allows me to delete the test user by Email
+async function deleteTestUser() {
+  await admin.auth()
+    .getUserByEmail('test@example.com')
+    .then((res) => {
+      if (res && res.uid) {
+        admin.auth().deleteUser(res.uid);
+      }
+    }).catch((err) => {
+      // If the user already doesn't exist just supress the error into a warning
+      console.warn(err);
+    });
+}
+
 before(async () => {
+  // Make sure the test user is gone before running tests
+  await deleteTestUser();
+
   await detox.init(config);
 });
 
